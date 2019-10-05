39a9794986bb2aabf3e03f55603f6dcf63821afc
sanjyot242 <sanjyot.satvi242.com>
2019-10-06 01:22:54 +0530

    Solved Error

--- app/src/main/java/com/example/numbershapes/MainActivity.java
+++ app/src/main/java/com/example/numbershapes/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     class Numbers
@@ -53,10 +54,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void numberShapes(View view){
         Log.i("info", "Button pressed");
-        Numbers Mynumber  =new Numbers();
         EditText editText = (EditText) findViewById(R.id.editText);
-        Mynumber.number= Integer.parseInt( editText.getText().toString());
-        if(isTriangular())
+        String message;
+        if(editText.getText().toString().isEmpty()){
+            message ="Please Enter a number";
+        }else {
+            Numbers Mynumber = new Numbers();
+            Mynumber.number = Integer.parseInt(editText.getText().toString());
+            message = editText.getText().toString();
+            if (Mynumber.isTriangular() && Mynumber.isSquare()) {
+                message += "is square and triangular";
+            } else if (Mynumber.isSquare()) {
+                message += "is square but not  triangular";
+            } else if (Mynumber.isTriangular()) {
+                message += "is triangular but not square";
+            }else{
+                message+="is neither triangular nor square";
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
